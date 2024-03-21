92fe45c2bb2cba7060c081b2063e285f674ca651
BaskarS1412 <bhaskars1412@gmail.com>
2024-03-21 20:55:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import DataComponent from "./Utils/DataComponent"; // Importing the DataComponent
+import CardComp from "./Component/CardComp"; // Importing the CardComp component
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Main App component
+const App = () => {
+  return (
+    <div>
+      <div>
+        {/* Wrapping CardComp inside DataComponent to provide product data */}
+        <DataComponent>
+          <CardComp />
+        </DataComponent>
+      </div>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default App; // Exporting the App component
